87934dba9d76702e0bcea9523aa4ffc29c8ba2f4
kirayomato <38139345+kirayomato@users.noreply.github.com>
2025-04-04 21:40:56 +0800

    移动目录

--- src/blrec/bili/test/wbi.py
+++ src/blrec/bili/test/wbi.py
@@ -3,6 +3,7 @@ from hashlib import md5
 import urllib.parse
 import time
 import requests
+
 headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en;q=0.3,en-US;q=0.2',  # noqa
