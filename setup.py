2f507907909af3ea4fa52d3d2a4b79c44db56e3b
kirayomato <38139345+kirayomato@users.noreply.github.com>
2026-01-28 10:37:57 +0800

    更新setup

--- setup.py
+++
@@ -1,6 +0,0 @@
-# This file is required for compatibility with older versions of pip/setuptools that don't support pyproject.toml configuration.
-
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
